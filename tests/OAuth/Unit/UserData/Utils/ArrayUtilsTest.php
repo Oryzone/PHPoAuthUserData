ea8d76e65a865ff1f6d6b3cb65906bc7616868d7
Luciano Mammino <lmammino@oryzone.com>
2014-02-07 17:58:42 +0100

    Added few unit tests and travis integration

---
+++ tests/OAuth/Unit/UserData/Utils/ArrayUtilsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace OAuth\Unit\UserData\Utils;
+
+
+use OAuth\UserData\Utils\ArrayUtils;
+
+class ArrayUtilsTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers Oauth\UserData\Utils\ArrayUtils::objectToArray
+     */
+    public function testObjectToArray()
+    {
+        $object = json_decode(
+            '{
+                "foo" : "bar",
+                "nested" : {
+                    "foo" : "bar"
+                },
+                "arr" : [1, 2, 3]
+            }'
+        );
+
+        $expected = array(
+            'foo' => 'bar',
+            'nested' => array(
+                'foo' => 'bar'
+            ),
+            'arr' => array(1, 2, 3)
+        );
+
+        $actual = ArrayUtils::objectToArray($object);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @covers Oauth\UserData\Utils\ArrayUtils::removeKeys
+     */
+    public function testRemoveKeys()
+    {
+        $array = array(
+            'foo' => 1,
+            'bar' => 2,
+            'baz' => 3,
+            'doo' => 4
+        );
+
+        $keys = array('foo', 'doo');
+
+        $expected = array(
+            'bar' => 2,
+            'baz' => 3
+        );
+
+        $actual = ArrayUtils::removeKeys($array, $keys);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+}
+ 
---
+++ tests/OAuth/Unit/UserData/Utils/StringUtilsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OAuth\Unit\UserData\Utils;
+
+
+use OAuth\UserData\Utils\StringUtils;
+
+class StringUtilsTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers OAuth\UserData\Utils\StringUtils::extractUrls
+     */
+    public function testExtractUrls()
+    {
+        $string = "Lorem ipsum http://example.com dolor sit http://example2.com amet http://example3.com";
+
+        $expected = array(
+            'http://example.com',
+            'http://example2.com',
+            'http://example3.com'
+        );
+
+        $actual = StringUtils::extractUrls($string);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+}
+ 
